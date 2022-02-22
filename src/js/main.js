4bd9900dd26c15bc50796cfde392e92fa0a0b39a
duclongpham <phamduclong2000192@gmail.com>
2022-02-22 23:05:20 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -45,4 +45,117 @@ $(function(){
             })
         })
     })();
+
+    ;(function() {
+        $('.showpopup').on('click', function(e) {
+            e.preventDefault();
+            let target = $(this).data('target');
+            $(target).fadeIn(function() {
+                $(target).addClass('show');
+                $(target).trigger('popup-opened');
+            });
+        })
+
+        $('body').on('click', '[data-close-popup]', function(e) {
+            let popup = $(this).closest('.popup');
+            if(!popup.length) return;
+            e.preventDefault();
+            popup.fadeOut(function() {
+                popup.removeClass('show');
+                popup.trigger('popup-closed');
+            });
+        })
+    })();
+
+    ;(function() {
+        class BaggageProcess {
+            constructor(element) {
+                this.$el = $(element);
+                this.tabs = this.$el.find('[data-tab]');
+                this.tabsData = Array.from(this.tabs).map(function(item) {
+                    let name = item.dataset.tab;
+                    return {
+                        name: name,
+                        $el: $(item),
+                        $tabTitle: $(element).find(`.process__header [data-target="${name}"]`),
+                        active: false
+                    }
+                })
+                this.tabsData[0].active = true;
+                this.isDone = false;
+                this.$process = this.$el.find('.progress.custom');
+                this.initEvent();
+                this.rerender();
+            }
+
+            initEvent() {
+
+            }
+
+            rerender() {
+                this.tabsData.forEach(function(item) {
+                    if(item.active) {
+                        item.$tabTitle.addClass('active');
+                        item.$el.addClass('active');
+                    } else {
+                        item.$tabTitle.removeClass('active');
+                        item.$el.removeClass('active');
+                    }
+                })
+
+                if(this.isDone) {
+                    this.$process.addClass('active');
+                }
+            }
+
+            nextProcess() {
+                let itemActive = null;
+                this.tabsData.forEach(function(item, index) {
+                    if(item.active) {
+                        itemActive =  index;
+                    }
+                });
+
+                if(itemActive === null) return ;
+                let next = itemActive + 1;
+                if(this.tabsData.length < next) return;
+                this.tabsData[next].active = true;
+                this.tabsData[itemActive].active = false;
+                if(this.tabsData.length === next) {
+                    this.isDone = true;
+                } else {
+                    this.isDone = false;
+                }
+                this.rerender();
+            }
+
+            prevProcess() {
+                let itemActive = null;
+                this.tabsData.forEach(function(item, index) {
+                    if(item.active) {
+                        itemActive =  index;
+                    }
+                });
+
+                if(itemActive === null) return ;
+                let prev = itemActive - 1;
+                if(prev < 0) return;
+                this.tabsData[prev].active = true;
+                this.tabsData[itemActive].active = false;
+
+                this.rerender();
+            }
+        }
+
+        $('.baggage-process').each(function(item) {
+            let instance = new BaggageProcess(this);
+            
+            $(this).find('.btn-outline-secondary').click(function(e) {
+                instance.nextProcess();
+            });
+            $(this).find('.btn-outline-warning').click(function(e) {
+                instance.prevProcess();
+            });
+        })
+    })();
 })
